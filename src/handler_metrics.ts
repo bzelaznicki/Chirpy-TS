1697eb80d4be3505e2cee366720fc04e95a829f2
bzelaznicki <bartosz@zelaznicki.com>
2025-05-12 22:31:39 +0200

    admin updates

--- src/handler_metrics.ts
+++ src/handler_metrics.ts
@@ -2,9 +2,14 @@ import { Request, Response } from "express";
 import { config } from "./config.js";
 
 export async function handlerMetrics(req: Request, res: Response){
-    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.set('Content-Type', 'text/html; charset=utf-8');
 
-    res.send(`Hits: ${config.fileserverHits}`); 
+    res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`); 
 }
 
 export async function handlerReset(req: Request, res: Response){
