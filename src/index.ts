bf45e88651c28b028b3c88fab84ac653a65a7925
bzelaznicki <bartosz@zelaznicki.com>
2025-05-12 20:07:44 +0200

    middleware logging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import path from "path";
 import { handlerReadiness } from "./handler_readiness.js";
+import { middlewareLogResponses } from "./middleware_logging.js";
 
 const app = express();
 const PORT = 8080;
 
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
 
