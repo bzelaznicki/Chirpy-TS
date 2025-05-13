23da96940f1503894963249441e35168aea4ac98
bzelaznicki <bartosz@zelaznicki.com>
2025-05-13 14:47:29 +0200

    database

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
+process.loadEnvFile();
 export type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 }
 
 export const config: APIConfig = {
     fileserverHits: 0,
+    dbURL: envOrThrow('DB_URL'),
 };
+
+function envOrThrow(key: string): string {
+    const value = process.env[key];
+    if (typeof value === 'undefined') {
+        throw new Error(`Environment variable ${key} is not set`);
+    }
+    return value;
+}
