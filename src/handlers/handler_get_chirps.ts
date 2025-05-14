04dffb3545c2bb191691f52d3d3e9701585e6982
bzelaznicki <bartosz@zelaznicki.com>
2025-05-14 09:00:02 +0200

    get single chirp

--- src/handlers/handler_get_chirps.ts
+++ src/handlers/handler_get_chirps.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
 import { respondWithJSON } from "../api/json.js";
-import { BadRequestError } from "../error_middleware.js";
-import { getChirps } from "../lib/db/queries/chirps.js";
+import { BadRequestError, NotFoundError } from "../error_middleware.js";
+import { getChirps, getSingleChirp } from "../lib/db/queries/chirps.js";
 
 
 export async function handlerGetChirps(req: Request, res: Response){
     const chirps = await getChirps();
 
     respondWithJSON(res, 200, chirps);
+}
+
+export async function handlerGetSingleChirp(req: Request, res: Response){
+    const chirpId = req.params.chirpId;
+
+    const chirp = await getSingleChirp(chirpId);
+
+    if (!chirp){
+        throw new NotFoundError(`Chirp ID ${chirpId} not found`)
+    }
+
+    respondWithJSON(res, 200, chirp);
 }
--- src/lib/db/queries/chirps.ts
+++ src/lib/db/queries/chirps.ts
@@ -1,6 +1,6 @@
 import { db } from "../index.js";
 import { NewChirp, chirps } from "../schema.js";
-import { asc, desc } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 
 export async function createChirp(chirp: NewChirp){
     const [result] = await db.insert(chirps).values(chirp).returning();
@@ -9,5 +9,10 @@ export async function createChirp(chirp: NewChirp){
 
 export async function getChirps(){
     const result = await db.select().from(chirps).orderBy(asc(chirps.createdAt));
+    return result;
+}
+
+export async function getSingleChirp(chirpId: string){
+    const [result] = await db.select().from(chirps).where(eq(chirps.id, chirpId));
     return result;
 }
